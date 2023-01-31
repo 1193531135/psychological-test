8be49c7bb0e868c58e22b0e31412cba9d1939f43
single <j1193531135@gmail.com>
2023-01-31 17:23:46 +0800

    init

---
+++ src/store/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import base from './base'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    strict: true,
+    state:{
+        username:'ddd',
+        ...base
+    },
+    mutations:{
+        modifyData(state,data){
+            state[data.name] = data.newdata
+        },
+        watchForMD(state,data){
+            if(data.constructor === Array){
+                state.moveDialogState[data[0]] = data[1]
+            }
+            state.moveDialogState[data.name] = data.newdata
+        },
+        showDomConfig(state,obj){
+            if(obj['type'] === 'add'){
+                state.showDom.push(obj.data)
+            }
+        }
+    }    
+})
